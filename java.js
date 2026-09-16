6b60fac08ea2411206ce540679f80257dbb4f56e
pedroarthur08 <pedroarthurcontato08@gmail.com>
2026-09-16 08:03:58 -0300

    Update java.js

--- java.js
+++ java.js
@@ -3,14 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (botao) {
     botao.addEventListener('click', () => {
-      // Alterna a classe no body
       document.body.classList.toggle('tema-escuro');
-
-      // Atualiza o texto do botão
       const estaEscuro = document.body.classList.contains('tema-escuro');
       botao.textContent = estaEscuro ? 'Ativar tema claro' : 'Ativar tema escuro';
     });
-  } else {
-    console.error('O botão com id="botaoTema" não foi encontrado no HTML.');
   }
 });
